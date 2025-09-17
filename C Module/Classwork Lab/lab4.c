dc1ac7f0234140f39ce7645f04db99f24eea535b
Himanshu Narkhede <92878676+Himanshu-014@users.noreply.github.com>
2025-09-17 15:42:14 +0530

    Update lab4.c

--- C Module/Classwork Lab/lab4.c
+++ C Module/Classwork Lab/lab4.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
-int main(){
-    int arr[6];
+#include<limits.h>
+void main() {
+    int arr[5];
+    int i, first, second;
 
     printf("Enter 5 values in the array:\n");
-    for (int i = 0; i < 5; i++)
-    {
+    for (i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
 
-    printf("Top 3 elements in array are:\n");
-    for (int i = 0; i < 6;i++){
-        
-        for (int j = 0; j < 6;j++){
-            while(j<= arr[i])
-			{
-				arr[i]= arr[i];
-                printf("%d",arr[i]);
-            }
+    first = second = INT_MIN; 
+
+    for (i = 0; i < 5; i++) {
+        if (arr[i] > first) {
+            second = first;                             // update second
+            first = arr[i];                            // update largest
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];                          // update second if it's not equal to largest
         }
-      
     }
+
+    if (second == INT_MIN) {
+        printf("No second largest element (all numbers are same).\n");
+    } else {
+        printf("Second largest element is: %d\n", second);
+    }
+
+   
 }
